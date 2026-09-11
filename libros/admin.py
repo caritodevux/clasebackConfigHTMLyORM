6fde49be928fbab485f8cba519a9ec4f6ea8ac5d
Carolina Inacap <c.rehbeindelrio@gmail.com>
2026-09-11 19:44:46 -0300

    modificamos admin.py para busqueda, filtro y personalización del admin

--- libros/admin.py
+++ libros/admin.py
@@ -3,4 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Libro
 
-admin.site.register(Libro)
+# admin.site.register(Libro)
+#Creamos una configuración personalizada para mostrar nuestro models en el admin
+class LibroAdmin(admin.ModelAdmin):
+    #Definimos las columnas que queremos ver en el listado
+    'id',
+    'titulo',
+    'autor',
+    'categoria',
+    'categorias_publicacion',
+    'disponible',
+
+    #activamos un buscador de libros
+    search_fields = ('titulo','autor',)
+
+    #activamos un filtro de registros
+    list_filter = ('categoria','disponible',)
+
+#Registramos la clase Model para el admin
+admin.site.register(Libro, LibroAdmin) 
