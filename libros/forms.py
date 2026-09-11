a03914e69de54510715a01bacad88a55e7a90ac2
Carolina Inacap <c.rehbeindelrio@gmail.com>
2026-09-11 20:27:54 -0300

    falta el form

---
+++ libros/forms.py
@@ -0,0 +1,18 @@
+#Generar formularios automaticos para un models
+from django import forms
+from .models import Libro
+
+# ModelForm permite crear automaticamente un formulario utilizando un models
+class LibroForm(forms.ModleForm):
+    #Metaclase
+    class Meta:
+        #a que modelo estamos apuntando
+        model = Libro
+        #indicamos que campos queremos ver en pantalla
+        fields = [
+            'titulo',
+            'autor',
+            'categoria',
+            'anio_publicacion',
+            'disponible',
+        ] 
--- libros/views.py
+++ libros/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Libro
+from .forms import LibroForm 
 
 # Create your views here.
+def listar_libros(request):
+    # Utilizamos Django ORM para obtener todos los libros alamcenados
+    libros = Libro.objects.all()
+    #render recibe 3 cosas: request, template a cargar, datos a utilizar
+    return render(
+        request, 
+        'libros/listar_libros.html',
+        {'libros'= libros}
+    )
